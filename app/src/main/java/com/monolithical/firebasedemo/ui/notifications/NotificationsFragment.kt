8c7c21877efd8b31df082f2e5bdfd19875abe06f
Remi de Boer <r.d.d.de.boer@hva.nl>
2020-11-10 11:29:57 +0100

    initial commit: firebase ready

---
+++ app/src/main/java/com/monolithical/firebasedemo/ui/notifications/NotificationsFragment.kt
@@ -0,0 +1,30 @@
+package com.monolithical.firebasedemo.ui.notifications
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Observer
+import com.monolithical.firebasedemo.R
+
+class NotificationsFragment : Fragment() {
+
+    private val notificationsViewModel: NotificationsViewModel by activityViewModels()
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+
+        val root = inflater.inflate(R.layout.fragment_notifications, container, false)
+        val textView: TextView = root.findViewById(R.id.text_notifications)
+        notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+}
